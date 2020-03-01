1839d602dded4b9a1e3a8cb23802220120cf42d4
taypal <tay.houk@yahoo.com>
2020-03-01 12:53:40 -0700

    DB setup and Config Setup

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name) VALUES ('Plain Burger');
+INSERT INTO burgers (burger_name) VALUES ('Bacon Cheese Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Turkey Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Veggie Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheese Burger with an egg', true);
---
+++ server.js
@@ -0,0 +1,3 @@
+var mysql = require("mysql");
+var express = require("express");
+var handlebars = require("express-handlebars");
